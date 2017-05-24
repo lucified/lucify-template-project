72ff47655eb92e0bb1f296cdb3204aeb7a1471ff
Ville Saarinen <ville.saarinen@gmail.com>
2017-05-24 15:49:01 +0300

    Add missing key props

--- src/template-utils.tsx
+++ src/template-utils.tsx
@@ -81,9 +81,9 @@ export function contextToHelmet(webpackCompilation: any) {
   return (
     <Helmet defaultTitle="Visualisation">
       <html lang="en" />
-      {assets.css.map((href: string) => <link rel="stylesheet" href={href} />)}
+      {assets.css.map((href: string) => <link key={href} rel="stylesheet" href={href} />)}
       {/*<link rel="canonical" href="https://www.lucify.com/something" />*/}
-      {assets.js.map((src: string) => <script type="text/javascript" src={src} />)}
+      {assets.js.map((src: string) => <script key={src} type="text/javascript" src={src} />)}
       <meta {...{charset: 'utf-8'} as any} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </Helmet>
